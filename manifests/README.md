ad4a5cadd8416f95d177f5d248155b4dca8999d6
Michael Tiemann <72577720+MichaelTiemannOSC@users.noreply.github.com>
2024-04-05 16:57:33 -0400

    Prepare for pre-commit
    Catch up with the latest releng standards for OS-Climate projects.
    
    Signed-off-by: Michael Tiemann <72577720+MichaelTiemannOSC@users.noreply.github.com>

--- manifests/README.md
+++ manifests/README.md
@@ -25,14 +25,17 @@ For a detailed guide on how to adjust your notebooks etc, please consult [docume
       gpg --keyserver keyserver.ubuntu.com --recv EFDB9AFBD18936D9AB6B2EECBD2C73FF891FBC7E
       ```
 
-   5. Import tcoufal's ([A76372D361282028A99F9A47590B857E0288997C](https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xa76372d361282028a99f9a47590b857e0288997c)) and mhild's [04DAFCD9470A962A2F272984E5EB0DA32F3372AC](https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x04dafcd9470a962a2f272984e5eb0da32f3372ac) keys (so they can help)
+   5. Import tcoufal's ([A76372D361282028A99F9A47590B857E0288997C](https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xa76372d361282028a99f9a47590b857e0288997c)) and mhild's
+      [04DAFCD9470A962A2F272984E5EB0DA32F3372AC](https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x04dafcd9470a962a2f272984e5eb0da32f3372ac) keys (so they can help)
 
       ```bash
       gpg --keyserver keyserver.ubuntu.com --recv A76372D361282028A99F9A47590B857E0288997C  # tcoufal
       gpg --keyserver keyserver.ubuntu.com --recv 04DAFCD9470A962A2F272984E5EB0DA32F3372AC  # mhild
       ```
 
-   6. If you'd like to be able to build the manifest on your own as well, please list your GPG key in the [`.sops.yaml` file](.sops.yaml), `pgp` section (add to the comma separated list). With your key present there, you can later generate the full manifests using `kustomize` yourself (`ksops` has to be installed, please follow ksops [guide](https://github.com/viaduct-ai/kustomize-sops#0-verify-requirements).
+   6. If you'd like to be able to build the manifest on your own as well, please list your GPG key in the [`.sops.yaml` file](.sops.yaml), `pgp` section (add to the comma separated list). With your
+      key present there, you can later generate the full manifests using `kustomize` yourself (`ksops` has to be installed, please follow ksops
+      [guide](https://github.com/viaduct-ai/kustomize-sops#0-verify-requirements).
 
 4. Create a secret and encrypt it with `sops`:
 
@@ -49,4 +52,5 @@ For a detailed guide on how to adjust your notebooks etc, please consult [docume
    sops --input-type=yaml --output-type=yaml -e /dev/stdin > ceph-creds.yaml
    ```
 
-Note: You can use the S2I image, that was built by [s2i-custom-notebook](https://github.com/AICoE/s2i-custom-notebook) for this automation. This image is expected to be used by default, therefore the `workingDir` is adjusted to `/opt/app-root/backup`. Please change or remove this settings in case you plan on using different image.
+Note: You can use the S2I image, that was built by [s2i-custom-notebook](https://github.com/AICoE/s2i-custom-notebook) for this automation. This image is expected to be used by default, therefore the
+`workingDir` is adjusted to `/opt/app-root/backup`. Please change or remove this settings in case you plan on using different image.
--- notebooks/common_units.py
+++ notebooks/common_units.py
@@ -1,18 +1,20 @@
-# Define common units shared by ITR ingest notebooks
-from pint import set_application_registry, Quantity, Context
-from pint_pandas import PintType
+"""Define common unites shared by ITR ingest notebooks."""
+
 from openscm_units import unit_registry
+from pint import Context, set_application_registry
+from pint_pandas import PintType
 
 PintType.ureg = unit_registry
 ureg = unit_registry
 set_application_registry(unit_registry)
 
 # Try to insert these definitions only once...
-if 'Steel' not in ureg:
+if "Steel" not in ureg:
     print("Initializing common units...")
-    # openscm_units doesn't make it easy to set preprocessors.  This is one way to do it.
-    unit_registry.preprocessors=[
-         lambda s1: s1.replace('passenger km', 'passenger_km'),
+    # openscm_units doesn't make it easy to set preprocessors.
+    # This is one way to do it.
+    unit_registry.preprocessors = [
+        lambda s1: s1.replace("passenger km", "passenger_km"),
     ]
 
     ureg.define("CO2e = CO2 = CO2eq = CO2_eq")
@@ -26,9 +28,9 @@ if 'Steel' not in ureg:
     ureg.define("Coal = [coal]")
 
     # These are for later
-    ureg.define('fraction = [] = frac')
-    ureg.define('percent = 1e-2 frac = pct = percentage')
-    ureg.define('ppm = 1e-6 fraction')
+    ureg.define("fraction = [] = frac")
+    ureg.define("percent = 1e-2 frac = pct = percentage")
+    ureg.define("ppm = 1e-6 fraction")
 
     ureg.define("USD = [currency]")
     ureg.define("EUR = nan USD")
@@ -38,7 +40,7 @@ if 'Steel' not in ureg:
     ureg.define("boe = 5.712 GJ = BoE")
     ureg.define("Mbbl = 1000 bbl")
     ureg.define("MMbbl = 1000000 bbl")
-    
+
     ureg.define("scf = ft**3")
     ureg.define("mscf = 1000 scf = Mscf")
     ureg.define("mmscf = 1000000 scf = MMscf")
@@ -52,17 +54,17 @@ if 'Steel' not in ureg:
     ureg.define("pkm = passenger * kilometer")
     ureg.define("tkm = tonne * kilometer")
 
-    ureg.define('hundred = 1e2')
-    ureg.define('thousand = 1e3')
-    ureg.define('million = 1e6')
-    ureg.define('billion = 1e9')
-    ureg.define('trillion = 1e12')
-    ureg.define('quadrillion = 1e15')
-    
-    i7y = Context('intensity')
-    i7y.add_transformation('Mscf CH4', 'kg CO2e', lambda ureg, x: x * ureg('54.87 kg CO2') / ureg('Mscf CH4'))
-    i7y.add_transformation('g CH4', 'g CO2e', lambda ureg, x: x * ureg('44 g CO2e') / ureg('16 g CH4'))
-    i7y.add_transformation('t Coal', 'MJ', lambda ureg, x: x * ureg('29288 MJ') / ureg('t Coal'))
-    i7y.add_transformation('t Coal', 't CO2e', lambda ureg, x: x * ureg('2.86 t CO2e') / ureg('t Coal'))
+    ureg.define("hundred = 1e2")
+    ureg.define("thousand = 1e3")
+    ureg.define("million = 1e6")
+    ureg.define("billion = 1e9")
+    ureg.define("trillion = 1e12")
+    ureg.define("quadrillion = 1e15")
+
+    i7y = Context("intensity")
+    i7y.add_transformation("Mscf CH4", "kg CO2e", lambda ureg, x: x * ureg("54.87 kg CO2") / ureg("Mscf CH4"))
+    i7y.add_transformation("g CH4", "g CO2e", lambda ureg, x: x * ureg("44 g CO2e") / ureg("16 g CH4"))
+    i7y.add_transformation("t Coal", "MJ", lambda ureg, x: x * ureg("29288 MJ") / ureg("t Coal"))
+    i7y.add_transformation("t Coal", "t CO2e", lambda ureg, x: x * ureg("2.86 t CO2e") / ureg("t Coal"))
     ureg.add_context(i7y)
-    ureg.enable_contexts('intensity')
+    ureg.enable_contexts("intensity")
